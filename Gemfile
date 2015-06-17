611602c941477bb2ae61758cbd7c92f53c9a5449
hideyuki-sakamoto <h0.sakamoto@gmail.com>
2015-06-17 16:35:03 +0900

    mofidy app.rb

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'libxml-ruby'
+gem 'aws-sdk'
 
 # gem "rails"
 #
--- app.rb
+++ app.rb
@@ -5,6 +5,24 @@ require 'sinatra/reloader'
 require './script/test.rb'
 require 'fileutils'
 
+require 'aws-sdk'
+
+creds = JSON.load(File.read("secrets.json"))
+Aws.config[:credentials] = Aws::Credentials.new(creds["AccessKeyId"], creds["SecretAccessKey"])
+Aws.config[:region] = "ap-northeast-1"
+
+def upload (src_fname, dst_fname)
+   file_open = File.open(src_fname)
+   s3 = Aws::S3::Client.new
+   #resp = s3.list_buckets
+   #puts resp.buckets.map(&:name)
+   s3.put_object(
+      bucket: "comet-cdc",
+      body: file_open,
+      key: dst_fname
+   )
+end
+
 get '/' do 
    erb :index
 end
@@ -15,22 +33,28 @@ post '/xml_upload' do
       today=Time.now
       dir_name = sprintf("%d%02d%02d",today.year, today.month, today.day)
 
-      save_path = "public/xml/#{dir_name}"
-      FileUtils.mkdir(save_path) unless File.exists?(save_path)
+      #save_path = "public/xml/#{dir_name}"
+      #FileUtils.mkdir(save_path) unless File.exists?(save_path)
       
-      File.open("#{save_path}/#{params[:file][:filename]}", "w") do |f|
-         f.write params[:file][:tempfile].read
-      end
+      #File.open("#{save_path}/#{params[:file][:filename]}", "w") do |f|
+      #   f.write params[:file][:tempfile].read
+      #end
+      
+      upload(params[:file][:tempfile], "xml/#{dir_name}/COMETCDC.xml")
 
       # generate daily/dir_name/data.json
-      write_entry(dir_name)
+      #write_entry(dir_name)
+      data_json = get_info(params[:file][:tempfile])
+      upload(data_json, "daily/#{dir_name}/data.json")
+      upload(data_json, "daily/current/data.json")
 
       # make link of current dir_name
-      FileUtils.rm("public/daily/current") if File.exists?("public/daily/current")
-      FileUtils.ln_s("#{dir_name}","public/daily/current")
+      #FileUtils.rm("public/daily/current") if File.exists?("public/daily/current")
+      #FileUtils.ln_s("#{dir_name}","public/daily/current")
 
       # generate stats/stats.json
-      write_stats
+      stats_json = get_stats
+      upload(stats_json, "stats/stats.json")
       
       redirect '/'
    end
@@ -38,9 +62,10 @@ end
 
 post '/csv_upload' do 
    if params[:file]
-      File.open("public/csv/#{params[:file][:filename]}", "w") do |f|
-         f.write params[:file][:tempfile].read
-      end
+      #File.open("public/csv/#{params[:file][:filename]}", "w") do |f|
+      #   f.write params[:file][:tempfile].read
+      #end
+      upload(params[:file][:tempfile], "csv/#{params[:file][:filename]}")
       redirect '/'
    end
 end
