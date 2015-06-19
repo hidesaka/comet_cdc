f3e2fb1bdd1854483c83b4174650741a21d1ad6e
sakah <h0.sakamoto@gmail.com>
2015-06-20 06:04:47 +0900

    debug done

--- public/js/file-upload.js
+++ public/js/file-upload.js
@@ -1,8 +1,7 @@
 function upload(name, url) {
-   $(name + " #progress_bar").attr("value", 0);
-   $(name + " #progress_bar").hide();
    $(name + " #progress_msg").html("");
 
+   $(name + " #progress_bar").attr("value", 0);
    $(name + " #progress_bar").show();
    $form = $(name);
    fd = new FormData($form[0]);
