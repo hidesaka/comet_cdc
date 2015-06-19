2bda94969b6c3adf4be5b667a8a766348639b039
hideyuki-sakamoto <h0.sakamoto@gmail.com>
2015-06-20 04:06:40 +0900

    change file uploader

--- public/js/file-upload.js
+++ public/js/file-upload.js
@@ -9,9 +9,20 @@ alert("start uploading");
          processData: false,
          contentType: false,
          data: fd,
-         success: function() {
-            console.log("sucess");
-         }
+         success: function() { console.log("sucess"); },
+         xhr : function(){
+            XHR = $.ajaxSettings.xhr();
+            if (XHR.upload){
+               XHR.upload.addEventListener('progress',function(e) {
+                     progre = parseInt(e.loaded/e.total*10000)/100 ;
+                        console.log(progre+"%") ;
+                        $("#progress_bar").width(parseInt(progre/100*300*100)/100+"px");
+                           $("#progress_bar").height("30px");
+                           $("#progress_bar").html(progre+"%");
+                     }, false); 
+                  }
+                  return XHR;
+               },
    });
    return false;
 }
