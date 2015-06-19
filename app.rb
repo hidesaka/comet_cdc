5088418807cf1b8d91c1c0a7e14adf926ea7ec48
hideyuki-sakamoto <h0.sakamoto@gmail.com>
2015-06-19 12:51:06 +0900

    add escape

--- app.rb
+++ app.rb
@@ -47,7 +47,7 @@ post '/xml_upload' do
          redirect '/'
 
       rescue => err
-         redirect "/err/#{err.message}"
+         redirect Rack::Utils.escape_html("/err/#{err.message}")
       end
    end
    redirect '/err/no xml file'
