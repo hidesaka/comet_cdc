ae0a58c1410ac788dc991b05f354161de9e9e04f
hideyuki-sakamoto <h0.sakamoto@gmail.com>
2015-06-19 22:23:15 +0900

    debug

--- app.rb
+++ app.rb
@@ -30,14 +30,14 @@ post '/xml_upload' do
          path = params[:file][:tempfile].path
          basename = File.basename(path)
 
-         today=Time.now
-         dir_name = sprintf("%d%02d%02d",today.year, today.month, today.day)
-         date = sprintf("%d/%02d/%02d",today.year, today.month, today.day)
-
-         s3_write("xml/#{dir_name}/COMETCDC.xml", body)
-         s3_write_daily_datum(date, date) # daily/20150611/data.json
-         s3_write_daily_stats(date, date) # daily/20150611/stat.json
-         s3_write_stats(date) # stats/stats.json
+#         today=Time.now
+#         dir_name = sprintf("%d%02d%02d",today.year, today.month, today.day)
+#         date = sprintf("%d/%02d/%02d",today.year, today.month, today.day)
+#
+#         s3_write("xml/#{dir_name}/COMETCDC.xml", body)
+#         s3_write_daily_datum(date, date) # daily/20150611/data.json
+#         s3_write_daily_stats(date, date) # daily/20150611/stat.json
+#         s3_write_stats(date) # stats/stats.json
 
          redirect '/'
 
