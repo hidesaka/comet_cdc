6aa213faec6daabea49a3c5583a9f3ddd21f35a4
sakah <h0.sakamoto@gmail.com>
2015-06-23 22:44:07 +0900

    fix done

--- public/js/wire.js
+++ public/js/wire.js
@@ -646,12 +646,12 @@ $(function () {
                                  var bar_data = read_tensionbar_csv(csv);
                                  for (i=0; i<dailies.length; i++) {
                                     for (j=0; j<bar_data.length; j++) {
-                                       console.log("i " + i);
-                                       console.log("j " + j);
-                                       console.log(" dailies.utime " + dailies[i].utime);
-                                       console.log("bar_data.utime " + bar_data[j].utime);
+                                       //console.log("i " + i);
+                                       //console.log("j " + j);
+                                       //console.log(" dailies.utime " + dailies[i].utime);
+                                       //console.log("bar_data.utime " + bar_data[j].utime);
                                        if (bar_data[j].utime > dailies[i].utime) {
-                                          console.log("breaked at i " + i + " j " + j);
+                                          //console.log("breaked at i " + i + " j " + j);
                                           j = j -1;
                                           break;
                                        }
