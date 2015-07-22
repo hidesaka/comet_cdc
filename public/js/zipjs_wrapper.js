39ad9da5b2f7d05e9ccc39cb7f5424fc718a6c3d
sakah <h0.sakamoto@gmail.com>
2015-07-22 13:03:09 +0900

    debug

--- public/js/zipjs_wrapper.js
+++ public/js/zipjs_wrapper.js
@@ -82,15 +82,15 @@
       obj.zipWrapper = function(id, callback) {
          var fileInput = $(id)[0];
          console.log("fileInput " + fileInput);
-         //var name = "#upload-xml";
-         //$(name + " #progress_msg").html("Compressing...");
-         //$(name + " #progress_bar").attr("value", 0);
-         //$(name + " #progress_bar").show();
 
          model.setCreationMethod("Blob");
 
          fileInput.addEventListener('change', function(event) {
                model.addFiles(fileInput.files, function() {
+                     var name = "#upload-xml";
+                     //$(name + " #progress_msg").html("Compressing...");
+                     //$(name + " #progress_bar").attr("value", 0);
+                     $(name + " #progress_bar").show();
                   }, function(file) {
                      var li = document.createElement("li");
                      //zipProgress.value = 0;
