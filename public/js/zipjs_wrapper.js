bc828789f72e8c67e2b860a6d112685725efd36d
sakah <h0.sakamoto@gmail.com>
2015-07-22 13:28:32 +0900

    move script from index.erb to zipjs_wrapper.js

--- public/js/zipjs_wrapper.js
+++ public/js/zipjs_wrapper.js
@@ -123,3 +123,47 @@
 
 })(this);
 
+$(function() {
+      zipWrapper("#upload-xml #upload-form-file", function(blob) {
+            var name = "#upload-xml";
+            var url = "/zip_upload";
+            $(name + " #progress_msg").show();
+            $(name + " #progress_bar").attr("value", 0);
+            $(name + " #progress_bar").show();
+            $(name + " #upload-form-file").attr("disabled","disabled");
+            console.log("starting ajax...");
+            console.log("blog: " + blob)
+            var fd = new FormData();
+            fd.append("zip", blob);
+            $.ajax({
+                  url: url,
+                  type: 'post',
+                  processData: false,
+                  contentType: false,
+                  data: fd,
+                  error: function (xhr, textStatus, errorThrown) {
+                     console.log("there is error at ajax...");
+                     console.log(xhr.responseText);
+                  },
+                  success: function(data) { 
+                     $(name + " #upload-form-file").val("").removeAttr("disabled");
+                     $(name + " #progress_msg").html("done!").fadeOut(3000);
+                     $(name + " #progress_bar").fadeOut(3000);
+                     console.log(data); 
+                  },
+                  xhr : function() {
+                     XHR = $.ajaxSettings.xhr();
+                     if (XHR.upload){
+                        XHR.upload.addEventListener('progress',function(e) {
+                              progre = parseInt(e.loaded/e.total*10000)/100 ;
+                                 //console.log(progre+"%") ;
+                                 $(name + " #progress_msg").height("30px");
+                                 $(name + " #progress_msg").html("Uploading.. " + progre+"%");
+                                 $(name + " #progress_bar").attr("value", progre);
+                           }, false); 
+                        }
+                        return XHR;
+                     },
+               });
+         });
+   });
