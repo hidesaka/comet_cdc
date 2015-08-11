3137e3bee01fe8c80fda55b56fcd297ba3c9e9f8
sakah <h0.sakamoto@gmail.com>
2015-08-12 02:23:40 +0900

    Use ticks(10). Number of ticks is fixed as 10

--- public/js/read.js
+++ public/js/read.js
@@ -191,7 +191,7 @@
       tick_label_dx = 0;
       tick_label_dy = 10;
       tick_label_rotate = "0";
-      xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(10, "%");
+      xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(10);
       if (options.xaxis_type === "time") {
         xAxis.ticks(5).tickFormat(d3.time.format('%b %d'));
         tick_label_rotate = "-65";
